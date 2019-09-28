df3f6384f928ece2cc9a41f6aa20fb690826cbba
CheJke <chejke@gmail.com>
2019-09-29 01:40:02 +0300

    добавлено формирование цсв-файла

--- spinningline_parser.py
+++ spinningline_parser.py
@@ -1,11 +1,16 @@
+import sys
 from bs4 import BeautifulSoup
-import xml
+import csv
+import os
 import urllib.request
 
 #функция для получения ссылок всех категорий в меню конкретной страницы
 def get_categ_urls(url, html):
-    categ_urls = ['https://spinningline.ru/primanki-blsny-c-38208_1.html?page=1&notstockcheck=true', 'https://spinningline.ru/primanki-voblery-c-38208_2.html']
+    categ_urls = []
+    for i in range(100):
+        categ_urls.append(f'https://spinningline.ru/primanki-blsny-c-38208_1.html?page={i+1}&notstockcheck=true')
     # !!!!!!!
+    print(categ_urls)
     return categ_urls
 
 #функция получения html из ссылки
@@ -17,9 +22,7 @@ def main():
     url = 'https://spinningline.ru/'
     html = get_html(url)
     categ_urls = get_categ_urls(url, html)
-    categ = []
-    prod = []
-    i = 0
+    prods_category = []
     # формируем словарь с характеристиками товака каждой категории
     for categ_url in categ_urls:
         categ_html = get_html(categ_url)
@@ -58,41 +61,19 @@ def main():
                 prod_property.update({
                     prop: val
                 })
+            prods_category.append(prod_property)
+        print(categ_url)
+        #создаем .csv файл
+    try:
+        csv_columns = prods_category[0].keys()
+        with open(f'{categ_name}.csv', 'w', encoding='utf-8-sig') as csv_file:
+            writer = csv.DictWriter(csv_file, fieldnames=csv_columns, delimiter=";")
+            writer.writeheader()
+            for prod in prods_category:
+                writer.writerow(prod)
 
-            print (prod_property)
-
-
-        # #начинаем заполнять товар и его характеристики.
-        # prod_page_list = soup.find_all('div', class_='b-prod__info-col')
-        # prod_img_list = soup.find_all('div', class_='b-prod__img-col')
-        # for prod_page in prod_page_list:
-        #     name = prod_page.find('div', class_='b-prod__name').text.lstrip().rstrip()
-        #     manufacturer = prod_page.find('div', class_='b-prod__manufacturer').text.lstrip().rstrip()
-        #     prod_props = prod_page.find_all('div', class_='b-prod-prop')
-        #     prod.append({
-        #         'Наименование': name,
-        #         'Производитель': manufacturer,
-        #     })
-        #     for prod_prop in prod_props:
-        #         prop = prod_prop.find('span', class_='b-prod-prop__name').text
-        #         val = prod_prop.find('span', class_='b-prod-prop__val').text
-        #         prod[-1].update({
-        #             prop: val
-        #         })
-        # # начинаем заполнять товар и его характеристики.
-        # for prod_img in prod_img_list:
-        #     img_small = prod_img.find('img').get('src')
-        #     prod_categ = str(prod_img.find('a').get('onclick')).replace("dataLayer.push({'event': 'ecommerce','EnchE': 'productClick','ecommerce' : {'click': {'products': [",'').replace(']}}});', '').replace(' / ','///')
-        #     prod_categ_dict = eval(prod_categ)
-        #     prod[i].update({
-        #         'img': img_small,
-        #         'Категория': prod_categ_dict['category']
-        #     })
-        #     i += 1
-
-
-    # print(prod)
-    # print(categ)
+    except Exception as e:
+        print(sys.exc_info())
 
 
 
