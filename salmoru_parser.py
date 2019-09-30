a31b5f7fe765615b6e8864245eed7e61b1664e8d
Chejke <chejke@gmail.com>
2019-09-30 17:38:11 +0300

    Старт парсинга салмору

---
+++ salmoru_parser.py
@@ -0,0 +1,24 @@
+from bs4 import BeautifulSoup
+import requests
+
+def get_html(url):
+    login = 'CheJke'
+    password = '123456'
+    s = requests.Session()
+    r = s.get(url, auth=(login, password))
+    return (r)
+
+
+def main():
+    url = 'http://www.salmoru.com/market/catalog/index.php'
+    r = get_html(url)
+    soup = BeautifulSoup(r.text, features="html.parser")
+    # table = soup.find('table', class_ = 'goodList')
+    table = soup.find_all('tr', class_ = 'bgr-white')
+    for prod_line in table:
+        print(prod_line)
+    print(len(table))
+
+
+if __name__ == '__main__':
+    main()
