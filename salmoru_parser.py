9011eaa501441c40f3860e3b7851d2f6974b8b41
Chejke <chejke@gmail.com>
2019-10-01 18:39:24 +0300

    Парсинг страницы с одним товаром

--- salmoru_parser.py
+++ salmoru_parser.py
@@ -11,14 +11,16 @@ def get_html(url):
 
 
 def main():
-    url = 'http://www.salmoru.com/market/catalog/index.php'
+    url = 'http://www.salmoru.com/market/catalog/index.php?pic=2&el_count=1'
     r = get_html(url)
     soup = BeautifulSoup(r.text, features="html.parser")
-    # table = soup.find('table', class_ = 'goodList')
-    table = soup.find_all('tr', class_ = 'bgr-white')
-    for prod_line in table:
-        print(prod_line)
-    print(len(table))
+    page_count = soup.find('div', class_='goodsOf').text.rstrip().split(' ').pop()
+    print(page_count)
+    main_prop = soup.find_all('tr', class_='bgr-blue2')
+    season = main_prop[0].text.lstrip().rstrip()
+    categoty = main_prop[1].text.lstrip().rstrip()[6:]+'///'+main_prop[2].text.lstrip().rstrip()[9:]
+
+
 
 
 if __name__ == '__main__':
